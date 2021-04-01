31b490b544eaf9c64d9b5bfadc7ae59534be913f
Nikita <nik.shchelkanov@gmail.com>
2021-04-01 13:10:06 +0300

    change contarct logic

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -5,55 +5,39 @@ pragma experimental ABIEncoderV2;
 contract Election {
 
     struct Candidate {
-        uint id;
         string name;
         uint voteCount;
     }
 
-    uint public candidateCount;
     uint public totalVotes;
     uint private maxVotes;
+    Candidate votingObject;
 
-    mapping(uint => Candidate) public candidateLookup;
     mapping(address => bool) public voterLookup;
 
-    event votedEvent(uint indexed id);
+    event votedEvent();
 
     function addCandidate(string memory name) public {
-        candidateLookup[candidateCount] = Candidate(candidateCount, name, 0);
-        candidateCount++;
+        votingObject = Candidate(name, 0);
     }
 
-    constructor(uint votersAmount) public {
+    constructor(string memory votingObjectName, uint votersAmount) public {
         require(votersAmount < 10);
         maxVotes = votersAmount;
-        addCandidate("kitty");
-        addCandidate("doggy");
+        addCandidate(votingObjectName);
     }
 
-    function getCandidate(uint id) external view returns (string memory name, uint voteCount) {
-        name = candidateLookup[id].name;
-        voteCount = candidateLookup[id].voteCount; 
+    function getCandidate() external view returns (string memory name, uint voteCount) {
+        name = votingObject.name;
+        voteCount = votingObject.voteCount; 
     }
 
-    function getCandidates() external view returns (string[] memory, uint[] memory) {
-        string[] memory names = new string[](candidateCount);
-        uint[] memory voteCounts = new uint[](candidateCount);
-        
-        for (uint i = 0; i < candidateCount; i++) {
-            names[i] = candidateLookup[i].name;
-            voteCounts[i] = candidateLookup[i].voteCount;
-        }
-        return (names, voteCounts);
-    }
-
-    function vote(uint id) external {
+    function vote() external {
         require(totalVotes < maxVotes);
         require(!voterLookup[msg.sender]);
-        require(id >= 0 && id <= candidateCount - 1);
-        candidateLookup[id].voteCount++;
+        votingObject.voteCount++;
         totalVotes++;
         voterLookup[msg.sender] = true;
-        emit votedEvent(id);
+        emit votedEvent();
     }
 }
