bc1ed5bc01ea738720437ceb8d0be354ac8d95ae
Nikita <nik.shchelkanov@gmail.com>
2021-04-01 15:19:55 +0300

    adjust test

--- tests/test_election.py
+++ tests/test_election.py
@@ -5,7 +5,6 @@ def test_vote(accounts, election_sc, Election, chain):
     election_sc = Election.deploy("Alise", 1, {"from": accounts[0]})
 
     election_sc.vote({"from": accounts[1]})
-    chain.sleep(86400 * 30)
 
     assert ("Alise", 1) == election_sc.getCandidate()
     assert election_sc.totalVotes() == 1
